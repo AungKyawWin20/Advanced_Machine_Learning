fa0df3e3f28194bdfcffe65c21b0cdc710bdca3f
[AungKyawWin20] <akw9785@gmail.com>
2025-03-23 17:37:12 +0700

    Add error handling to loan prediction API and improve code formatting

--- flask_apps/Loan_Flask/loan_pred.py
+++ flask_apps/Loan_Flask/loan_pred.py
@@ -1,25 +1,23 @@
-#Importing necessary libraries
+# Importing necessary libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
 
-#Loading the dataset       
+# Loading the dataset
 df = pd.read_csv(r'C:\Users\akw97\Desktop\Parami Courses\Advanced_Machine_Learning\flask_apps\Loan_Flask\loan_data.csv')
 
-#Dropping the 'loan_status' column
+# Dropping the 'loan_status' column
 X = df.drop('loan_status', axis=1)
-
 y = df['loan_status']
 
-#Label Encoding the categorical columns and standardizing the numerical columns
+# Initialize LabelEncoder and StandardScaler
 le = LabelEncoder()
 sc = StandardScaler()
 
+# Label Encoding for categorical columns
 categorical_cols = X.select_dtypes(include='object').columns
 for col in categorical_cols:
     X[col] = le.fit_transform(X[col])
@@ -27,24 +25,22 @@ for col in categorical_cols:
 # Standardizing numerical columns
 numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns
 X[numerical_cols] = sc.fit_transform(X[numerical_cols])
-        
-#Splitting the dataset into training and testing sets
+
+# Splitting the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, shuffle=True, stratify=y)
 
-#Using KNN Classifier to train the model
-#KNN Classifier with 5 neighbors, manhattan distance metric, distance weights and p=2
-knn = KNeighborsClassifier(n_neighbors=5, metric='manhattan', weights = 'distance', p=2)
-
+# Using KNN Classifier to train the model
+knn = KNeighborsClassifier(n_neighbors=5, metric='manhattan', weights='distance', p=2)
 knn.fit(X_train, y_train)
 
-#Exporting the model
+# Exporting the model
 joblib.dump(knn, 'loan_pred_model.joblib')
 print('Model has been successfully exported')
 
-#Exporting the Label Encoder
+# Exporting the Label Encoder
 joblib.dump(le, 'label_encoder.joblib')
 print('Label Encoder has been successfully exported')
 
-#Exporting the Standard Scaler
+# Exporting the Standard Scaler
 joblib.dump(sc, 'standard_scaler.joblib')
 print('Standard Scaler has been successfully exported')
